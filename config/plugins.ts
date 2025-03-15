16a15ce8aaee99c934f35ef41ddc02eb32763760
Alejandro Anibal Andrade <alejandro21112@hotmail.com>
2025-03-15 15:19:48 -0500

    sin el plug de neon

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,10 @@
-export default {
-    "strapi-neon-tech-db-branches": {
-      enabled: true,
-      config: {
-        neonApiKey: "napi_qfv9qxzkgh69kk9cvtlg1nz7wi77tiyh45wm9wiv9w2ws63li122sw3mkhkiga0l",
-        neonProjectName: "backend-ecom",
-        neonRole: "neondb_owner",
-      },
-    },
-  }
+// export default {
+//     "strapi-neon-tech-db-branches": {
+//       enabled: true,
+//       config: {
+//         neonApiKey: "napi_qfv9qxzkgh69kk9cvtlg1nz7wi77tiyh45wm9wiv9w2ws63li122sw3mkhkiga0l",
+//         neonProjectName: "backend-ecom",
+//         neonRole: "neondb_owner",
+//       },
+//     },
+//   }
