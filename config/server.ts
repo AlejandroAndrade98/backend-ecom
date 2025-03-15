731056c8fffcb0a7224631808fc09b4c0539fed1
Alejandro Anibal Andrade <alejandro21112@hotmail.com>
2025-03-15 17:35:33 -0500

    ägregando cambios en server

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,8 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // En producción, configura APP_KEYS en Render con una lista de claves separadas por comas.
+    // Ejemplo en Render: APP_KEYS=clave1,clave2,clave3,clave4
+    keys: env.array('APP_KEYS', ['clave1', 'clave2', 'clave3', 'clave4']),
   },
 });
